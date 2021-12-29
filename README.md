adef61a5e59cb6d05047fb31f831ac7118880d63
codermauro <mauro.druwel@gmail.com>
2021-12-29 17:05:52 +0100

    Add Structure

---
+++ README.md
@@ -0,0 +1,25 @@
+# WPSE352 photosensitive sensor module
+
+## Example 1
+This example shows how you can use the Whadda WPSE352 photosensitive sensor module with an Arduino® compatible board.
+The example program reads the digital light signal value from the ```D2``` pin on the module and turns on an led.
+
+### Library dependencies
+* None
+
+### Wiring diagram
+![](./wiring_diagram_bb.png)
+
+## Example 2
+This example shows how you can use the Whadda WPSE352 photosensitive sensor module with an Arduino® compatible board.
+The example program reads the digital light signal value from the ```A0``` pin on the module and prints the result in the serial monitor.
+
+### Library dependencies
+* None
+
+### Wiring diagram
+![](./wiring_diagram_bb.png)
+
+## Additional information
+  For more information about the Whadda WPSE352 photosensitive sensor module, check the manual available at [whadda.com](https://whadda.com)
+
---
+++ examples/WPSE352_example2.ino
@@ -0,0 +1,37 @@
+/*
+  @@@@@@@@@@@@@@@@@@@@@@                                                                                                                               
+  @@@@@@@@@@@@@@@@@@@@@@             @@@.    @@@    @@@.     @@@@    @@@@        @@@@@@@@@        @@@@@@@@@@@@@.     .@@@@@@@@@@@@@        @@@@@@@@@  
+  @@@@@%%@@@%%@@@%%@@@@@             @@@@    @@@    @@@@     @@@@    @@@@       @@@@@@@@@@        @@@@@@@@@@@@@@     @@@@@@@@@@@@@@        @@@@@@@@@  
+  @@@@@  @@@  @@@  @@@@@             @@@@    @@@    @@@@     @@@@    @@@@       @@@@   @@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@   @@@@ 
+  @@@@@            @@@@@             @@@@   @@@@    @@@@     @@@@@@@@@@@@       @@@@   @@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@   @@@@ 
+  @@@@@   @    @   @@@@@             @@@@   @@@@    @@@@     @@@@@@@@@@@@       @@@@@@@@@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@@@@@@@@ 
+  @@@@@            @@@@@             @@@@###@@@@@##@@@@@     @@@@    @@@@      @@@@@@@@@@@@       #@@@@@###@@@@@     ##@@@@@###@@@@       @@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@             @@@@@@@@@@@@@@@@@@@     @@@@    @@@@      @@@@    @@@@@      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@      @@@@     @@@@
+  @@@@@@@@@@@@@@@@@@@@@@
+  
+  Whadda PHOTOSENSITIVE SENSOR MODULE
+  
+ */
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the light sensor is attached to
+
+int sensorValue = 0;        // value read from the light sensor
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+
+  // print the results to the Serial Monitor:
+  Serial.print("sensor = ");
+  Serial.println(sensorValue);
+
+  // wait 10 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(10);
+}
